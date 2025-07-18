47140ef07b3a8856c77dddc03fb94761bc3dc7b9
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-07-18 21:39:40 +0530

    Add main plugin file with class loader

---
+++ course-progress-tracker.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Course Progress Tracker
+ * Description: A custom plugin to track users’ course progress.
+ * Version: 1.0.0
+ * Author: Sudhir Gaikwad
+ * License: GPL2
+ */
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+// Define constant for plugin path
+define('CPT_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+// Include main class
+require_once CPT_PLUGIN_PATH . 'includes/class-course-tracker.php';
+
+// Initialize plugin
+function cpt_init_plugin() {
+    $plugin = new Course_Progress_Tracker();
+    $plugin->init();
+}
+add_action('plugins_loaded', 'cpt_init_plugin');
